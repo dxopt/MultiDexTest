b000385eefc158cef96e95a98cc477ee5f1e0a1d
Yongce Tu <yongce.tu@gmail.com>
2014-11-06 00:37:21 +0800

    change: Add demo to reproduce the multidex bug

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-MultidexTest
+MultiDexTest
 ============
+
+This app was written to reproduce a bug in Android build tools.
+
+
+Reproduce environment
+============
+
+1. Gradle plugin: com.android.tools.build:gradle:0.14.0
+2. Compile SDK: android-21
+3. SDK Build tools: 21.1.0
+4. SDK tools: 23.0.5
+5. SDK platform-tools: 21
+6. Ubuntu & Android Studio 0.9.1
+
+Bug descrition
+============
+
+In latest SDK build tools, we can use the dx parameters '--multi-dex' & '--main-dex-list' & '--minimal-main-dex' to generate and control multiple dex files in APK. If these parameters were used, the expected dex files will be generated, it's great. But in the final APK, the dex files' names were changed.
+
+From the following operation logs, you can see that the "classes*.dex" names in the final APK were exchanged!
+
+```
+[PWD: ~/work/tyc/ycdev/demos/MultiDexTest/app]  (master)
+$ ../gradlew clean aDebug
+:app:clean
+<...ignored...>
+:app:assembleDebug
+
+BUILD SUCCESSFUL
+
+Total time: 38.889 secs
+[PWD: ~/work/tyc/ycdev/demos/MultiDexTest/app]  (master)
+$ ll build/intermediates/dex/debug/
+总用量 4280
+drwxrwxr-x 2 yongce yongce    4096 11月  6 00:16 ./
+drwxrwxr-x 3 yongce yongce    4096 11月  6 00:16 ../
+-rw-rw-r-- 1 yongce yongce  874148 11月  6 00:16 classes2.dex
+-rw-rw-r-- 1 yongce yongce 3494932 11月  6 00:16 classes.dex
+[PWD: ~/work/tyc/ycdev/demos/MultiDexTest/app]  (master)
+$ unzip -l build/outputs/apk/app-debug.apk | grep ".dex"
+   874148  2014-11-06 00:16   classes.dex
+  3494932  2014-11-06 00:16   classes2.dex
+```
---
+++ app/src/main/java/me/ycdev/android/demo/multidextest/MainActivity.java
@@ -0,0 +1,51 @@
+package me.ycdev.android.demo.multidextest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // use reflection since the jar project is in the packaging scope but not
+        // the compile scope.
+        try {
+            for (int i = 1 ; i <= 70 ; i++) {
+                Class<?> clazz = getClassLoader().loadClass(String.format("com.android.tests.basic.manymethods.Big%03d", i));
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
